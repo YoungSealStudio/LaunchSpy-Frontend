882380ec9a647a171223f69c6f852458289b2faf
locke <hayeong28@naver.com>
2024-03-31 18:19:20 +0900

    feat: 로그인 description 추가

--- src/routes/login.lazy.tsx
+++ src/routes/login.lazy.tsx
@@ -1,5 +1,4 @@
 import { Link, createLazyFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
 
 import UserAuthForm from '@/app/components/user-auth-form'
 import { Button } from '@/components/ui/button'
@@ -16,27 +15,29 @@ const gsiClientResource = makeGsiClientResource()
 function Login() {
   const gsiClient = gsiClientResource.read()
 
-  const userAuthFormProps = useUserAuthForm({
+  const { isEmailSignIn, ...userAuthFormProps } = useUserAuthForm({
     gsiClient,
   })
 
-  const handleJoinClick = () => {}
-
   return (
-    <div className="flex items-center justify-center h-screen w-full">
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <div className="flex flex-col items-center justify-center mb-10">
+        <h2 className="text-3xl	mb-5">다른 회사의 맛집을 추천받아 볼까요?</h2>
+        <p className="text-slate-500	">
+          {isEmailSignIn
+            ? '비밀번호를 입력해주세요'
+            : '먼저 이메일 입력이 필요해요'}
+        </p>
+      </div>
       <div className="flex flex-col items-center justify-center">
         <Card className="p-2 w-[400px]">
-          <UserAuthForm {...userAuthFormProps} className="w-full" />
+          <UserAuthForm
+            {...userAuthFormProps}
+            className="w-full"
+            emailPlaceholder="여기에 이메일 입력"
+            isEmailSignIn={isEmailSignIn}
+          />
         </Card>
-
-        <Button
-          className="mt-10 p-0 box-border w-[calc(100%_-_60px)] bg-gray-400 h-fit"
-          onClick={handleJoinClick}
-        >
-          <Link className="w-full h-full p-3" to="/join">
-            회원가입
-          </Link>
-        </Button>
       </div>
     </div>
   )
